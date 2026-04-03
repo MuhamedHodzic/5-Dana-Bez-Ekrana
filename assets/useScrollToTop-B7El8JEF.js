3ba04e3761bc635a59e8e7c9266fcbaca89d3e75
MuhamedHodzic <MuhamedHodzic@users.noreply.github.com>
2026-04-03 20:45:31 +0000

    deploy: 83b856c4cc8cc1287c6a9bdfa5c2236f5318bfe3

---
+++ assets/useScrollToTop-B7El8JEF.js
@@ -0,0 +1,2 @@
+import{u as s,r as t}from"./index-BNQ5zw_X.js";function a(){const{pathname:o}=s();t.useEffect(()=>{window.scrollTo(0,0)},[o])}export{a as u};
+//# sourceMappingURL=useScrollToTop-B7El8JEF.js.map
--- assets/useScrollToTop-CpkXWvVm.js
+++
@@ -1,2 +0,0 @@
-import{u as s,r as t}from"./index-CykZwGqs.js";function a(){const{pathname:o}=s();t.useEffect(()=>{window.scrollTo(0,0)},[o])}export{a as u};
-//# sourceMappingURL=useScrollToTop-CpkXWvVm.js.map

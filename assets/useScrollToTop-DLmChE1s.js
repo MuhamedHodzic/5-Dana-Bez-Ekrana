34dc8749bced4ded3465101246910fbc5bfe09f3
MuhamedHodzic <MuhamedHodzic@users.noreply.github.com>
2026-04-03 20:49:27 +0000

    deploy: 8b048ef4fb9734a194b2051b53fb134ee0b9081c

---
+++ assets/useScrollToTop-DLmChE1s.js
@@ -0,0 +1,2 @@
+import{u as s,r as t}from"./index-MGcHNTpU.js";function a(){const{pathname:o}=s();t.useEffect(()=>{window.scrollTo(0,0)},[o])}export{a as u};
+//# sourceMappingURL=useScrollToTop-DLmChE1s.js.map

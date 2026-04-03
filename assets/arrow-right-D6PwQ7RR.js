b8cbe36f74e90dbccfe8366f3a7a8067e38cc27f
MuhamedHodzic <MuhamedHodzic@users.noreply.github.com>
2026-04-03 19:22:20 +0000

    deploy: ed04fdc7b8de5d5711271e82466f61406c6817e1

--- assets/arrow-right-D6PwQ7RR.js
+++
@@ -1,2 +0,0 @@
-import{c as o}from"./index-vvPC3lUP.js";const r=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],c=o("arrow-right",r);export{c as A};
-//# sourceMappingURL=arrow-right-D6PwQ7RR.js.map
---
+++ assets/useScrollToTop-CVv-2JQ-.js
@@ -0,0 +1,2 @@
+import{u as s,r as t}from"./index-mkt8B7Sm.js";function a(){const{pathname:o}=s();t.useEffect(()=>{window.scrollTo(0,0)},[o])}export{a as u};
+//# sourceMappingURL=useScrollToTop-CVv-2JQ-.js.map

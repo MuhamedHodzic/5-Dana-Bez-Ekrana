53ca293154ea69e82f7860223460d1567807e6ef
MuhamedHodzic <MuhamedHodzic@users.noreply.github.com>
2026-04-03 19:05:58 +0000

    deploy: 45ee83de4d84c391754ade7a215dbfe416879e5d

--- client/assets/useScrollToTop-D1Hx65yK.js
+++
@@ -1,2 +0,0 @@
-import{u as s,r as t}from"./index-CyeNVTI-.js";function a(){const{pathname:o}=s();t.useEffect(()=>{window.scrollTo(0,0)},[o])}export{a as u};
-//# sourceMappingURL=useScrollToTop-D1Hx65yK.js.map

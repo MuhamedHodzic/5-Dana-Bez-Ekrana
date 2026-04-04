99aefebe63a9bb306ee36002603a521a1beed4eb
MuhamedHodzic <MuhamedHodzic@users.noreply.github.com>
2026-04-04 00:27:06 +0000

    deploy: ca110f57075822fca62b82bb06b4dd81cfc3cd5c

---
+++ assets/useScrollToTop-BvHhM2Rz.js
@@ -0,0 +1,2 @@
+import{u as s,r as t}from"./index-B17SpqxF.js";function a(){const{pathname:o}=s();t.useEffect(()=>{window.scrollTo(0,0)},[o])}export{a as u};
+//# sourceMappingURL=useScrollToTop-BvHhM2Rz.js.map
